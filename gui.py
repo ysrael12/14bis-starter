36f6045913a98c05b5750f92b36bcb212011862c
ysrael12 <85563735+ysrael12@users.noreply.github.com>
2023-12-03 10:51:56 -0300

    easter eg

--- gui.py
+++ gui.py
@@ -2,15 +2,18 @@ from getIp import browse_server, get_ip_address
 from makeBackup import fazerbackup
 import tkinter as tk
 import qrcode
+from tkinter import messagebox
 from PIL import Image, ImageTk
 
 
+def show_notification():
+    messagebox.showinfo("Easter Egg", "Raposo não pegue !")
+    
 def makebackup():
     fazerbackup()
 
 def start_btn_click():
     browse_server()
-
 
 def generate_qr_code(link):
     qr = qrcode.QRCode(
@@ -24,16 +27,18 @@ def generate_qr_code(link):
     qr_img = qr.make_image(fill_color="black", back_color="white")
     return qr_img
 
-
 def create_gui(window):
     frame = tk.Frame(window)
     frame.pack(expand=True, fill='both')
 
     mkbackup = tk.Button(frame, padx=20, pady=10, text="Fazer Backup", command=makebackup,  bg='blue', fg='white')
     start_btn = tk.Button(frame, padx=20, pady=10, text="Abrir Biblivre V", command=start_btn_click, bg='green', fg='white')
-
+    button = tk.Button(window, text="Easter Egg", command=show_notification, padx=20, pady=10,  bg='red', fg='white'  )
+    
+    button.pack(pady=20)
     mkbackup.pack(pady=20)  # Adjusted padding for "Fazer Backup" button
     start_btn.pack(pady=20)  # Adjusted padding for "Ver link" button
+
 
     link = 'http://{}/Biblivre5/'.format(get_ip_address())  # Replace with your link
     qr_img = generate_qr_code(link)
@@ -48,10 +53,11 @@ def create_gui(window):
 
 def create_window():
     window = tk.Tk()
-    window.geometry("500x500")  # Adjusted window size for better visibility
+    window.geometry("600x600")  # Adjusted window size for better visibility
     window.title("14 Bis starter")
+    #window.iconbitmap('exe/14bis.ico')
     create_gui(window)
-    
-    window.mainloop()
+    window.mainloop() 
+  
 
 create_window()
